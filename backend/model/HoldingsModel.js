172de6c24d00d437f69514e5e637b67866953900
Harshvardhan Rawat <fest.nov01@gmail.com>
2026-05-15 16:54:36 +0530

    Initial frontend commit

--- backend/model/HoldingsModel.js
+++
@@ -1,6 +0,0 @@
-const {model} = require('mongoose');
-const {HoldingSchema} = require('../schemas/HoldingSchema');
-
-const HoldingModel = new model('Holding', HoldingSchema);
-
-module.exports = { HoldingModel };
